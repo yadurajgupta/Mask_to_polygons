399fe97a843e052e9e6a18da43663b4d3d692d92
yadurajgupta <36245975+yadurajgupta@users.noreply.github.com>
2020-05-24 23:16:38 +0530

    Push

--- test.py
+++ test.py
@@ -1,15 +1,27 @@
 import cv2 as cv
 import mask_to_plot as msk
-img_path="C:\\Users\\yadur\\Downloads\\Edge\\Dataset\\ADEChallengeData2016\\images\\training\\"
-mask_path="C:\\Users\\yadur\\Downloads\\Edge\\Dataset\\ADEChallengeData2016\\annotations\\training\\"
-img_name="ADE_train_00000010"
+from numba import jit
+import timeit
 
-img=cv.imread(img_path+img_name+".jpg")
-mask=cv.imread(mask_path+img_name+".png")
+img_path="C:\\Users\\yadur\\Downloads\\Edge\\Dataset\\ADEChallengeData2016\\images\\validation\\"
+mask_path="C:\\Users\\yadur\\Downloads\\Edge\\Dataset\\ADEChallengeData2016\\annotations\\validation\\"
+overlay_path="C:\\Users\\yadur\\Downloads\\Edge\\Dataset\\ADEChallengeData2016\\Overlay\\validation\\"
+base="ADE_val_"
+def func():
+	for i in range(10):
+		num=str(i+1)
+		while len(num)<8:
+			num="0"+num;
+		img_name=base+num
+		img=cv.imread(img_path+img_name+".jpg")
+		mask=cv.imread(mask_path+img_name+".png")
+		contours=msk.give_contours(mask)
+		newimg=msk.contours_overlay(contours,img)
+		ret=cv.imwrite(overlay_path+img_name+".png",newimg)
+		if ret:
+			print(i)
 
-contours=msk.give_contours(mask)
-newimg=msk.contours_overlay(contours,img)
-msk.visualize_mask(mask)
-cv.imshow('output',newimg)
+func()
+print("YES")
 cv.waitKey(0);
 cv.destroyAllWindows()
